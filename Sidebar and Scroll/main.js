ea193db9dec7806afa99f4e906dc6a53bbffeb0c
Nur Hossain Arman <nh.arman30@gmail.com>
2023-02-04 21:43:16 +0600

    update

--- Sidebar and Scroll/main.js
+++ Sidebar and Scroll/main.js
@@ -1,9 +1,13 @@
 const toggleButton = document.getElementById("toggle");
 const sidebar = document.getElementById("sidebar");
+const details = document.getElementById("details-container");
+
+
+var sidebarShowing = false;
 
 toggleButton.onclick = function() {
-  
-  sidebar.classList.toggle("active");  
+  sidebar.classList.toggle("active");
+  details.classList.toggle("active");
 };
 
 
